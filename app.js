7e5b2329904a233a0bfdc204ce08b73fe5596255
TheBetterLuis <121691688+TheBetterLuis@users.noreply.github.com>
2025-02-08 18:46:43 -0400

    frontend setup

--- app.js
+++ app.js
@@ -14,6 +14,27 @@ app.use(express.json());
 
 //app.use("/images", express.static(path.join(__dirname, "images")));
 
+//front end
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve static files from the client directory
+app.use(express.static(path.join(__dirname, "client")));
+
+// Define routes for different pages
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "contact.html"));
+});
+//front end
+
 app.use("/api/", require("./routes"));
 app.listen(PORT, () => {
   console.log("api is ready");
